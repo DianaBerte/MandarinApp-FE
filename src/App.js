6934ba370d6cf78010d378ff72e6c07f926989bd
DianaBerte <dianaberte.go@gmail.com>
2023-04-26 15:30:46 +0200

    Initial setup - displays app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+    return ("teeeeeeeeeeeeeeeeeeeeeeeeest"
+        // <BrowserRouter>
+        //     <Routes>
+        //         <Route path="/" />
+        //     </Routes>
+        // </BrowserRouter>
+    );
+}
+
+export default App;
---
+++ src/redux/store/index.js
@@ -0,0 +1,28 @@
+import Storage from "redux-persist/lib/storage";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import userReducer from "../reducers/userReducer.js";
+
+const persistConfig = {
+    storage: Storage,
+    key: "root",
+}
+
+const combinedReducer = combineReducers({
+    users: userReducer,
+})
+
+const persistedReducer = persistReducer(persistConfig, combinedReducer);
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) => {
+        return getDefaultMiddleware({
+            serializableCheck: false,
+        });
+    },
+});
+
+const persistedStore = persistStore(store)
+
+export { store, persistedStore }
