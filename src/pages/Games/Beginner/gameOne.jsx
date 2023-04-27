8e3d3d5c0eec4b80251a94209c1398d274c55668
DianaBerte <dianaberte.go@gmail.com>
2023-04-27 16:34:35 +0200

    Tentative connection to BE

--- src/pages/Games/Beginner/gameOne.jsx
+++ src/pages/Games/Beginner/gameOne.jsx
@@ -1,18 +1,20 @@
 import { Container, Button } from "react-bootstrap";
 import { useState } from "react";
-
-
-
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const GameOne = () => {
 
-
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     const playGameOne = async () => {
         try {
-            
+            let res = await fetch(`${process.env.REACT_APP_BE_URL}/games`);
+            let data = await JSON.parse(res);
+            console.log("data: ", data)
         } catch (error) {
-            console.log(error)
+            console.log(error)  
         }
     };
 
@@ -23,6 +25,11 @@ const GameOne = () => {
                 <div className="game-container">
                     <div className="left-column"></div>
                     <div className="right-column"></div>
+                </div>
+                <div className="btn-wrapper">
+                    <Button className="play" onClick={playGameOne}>
+                      Play
+                    </Button>
                 </div>
             </div>
         </Container>
--- src/redux/hooks/index.js
+++
@@ -1,7 +0,0 @@
-import { useSelector, useDispatch } from "react-redux";
-
-// Select state from Redux store
-export const useDefaultSelector = (selector) => useSelector(selector);
-
-// Dispatch actions to the store
-export const useDefaultDispatch = () => useDispatch();
