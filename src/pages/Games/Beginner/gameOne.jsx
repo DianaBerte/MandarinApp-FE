d7417476f3d26b0b3350f7e5b8f20c751f4c3e28
DianaBerte <dianaberte.go@gmail.com>
2023-05-02 11:23:20 +0200

    Displaying answers

--- src/pages/Games/Beginner/gameOne.jsx
+++ src/pages/Games/Beginner/gameOne.jsx
@@ -1,12 +1,12 @@
 import { Container, Button } from "react-bootstrap";
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useAppDispatch, useAppSelector } from "../../../redux/hooks/index.js";
 import { useNavigate } from "react-router-dom";
 import { SET_CURRENT_GAME, setCurrentGame } from "../../../redux/actions/index.js";
 
 const GameOne = () => {
     let [game, setGame] = useState("")
+
     const games = useSelector((state) => {
         return state.currentGame;
     });
@@ -18,8 +18,7 @@ const GameOne = () => {
         dispatch(setCurrentGame());
     }, [])
 
-    const playGameOne = async () => {   
-        console.log("hello")         
+    const playGameOne = async () => {          
         try {
             // const currentGame = await res.json();
             dispatch(setCurrentGame())
@@ -36,16 +35,29 @@ const GameOne = () => {
         <Container>
             <div className="container-div">
                 <div className="game-container">
-                    <div className="left-column">                        
-                        {/* <p>here I want to display questions, which is array of strings</p> */}
+                    <div className="left-column">
+                        <h5>Questions</h5>
+                        <ul>
+                            {games.map(game => (
+                                <li key={game._id}>{game.questions}</li>
+                            ))}
+                        </ul>
                     </div>
                     <div className="right-column">
-                        {/* <p>here I want to display answers, which is an array containing questionSchema</p> */}
+                        <h5>Answers</h5>
+                        <ul>
+                            {games.map((game) =>
+                            game.answers.map((answer) => 
+                            answer.answers.map((ans) => (
+                                <li key={ans}>{ans}</li>
+                            ))
+                            )    
+                        )}
+                        </ul>
                     </div>
                 </div>
                 <div className="btn-wrapper">
                     <Button className="play" onClick={playGameOne}>
-                        {/* playGameOne will fetch data from BE, and it will display questions in the left column and answers in the right column */}
                       Play
                     </Button>
                 </div>
