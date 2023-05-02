025f1cb528ca5197625497a7886e9bff2985ab7e
DianaBerte <dianaberte.go@gmail.com>
2023-05-02 13:15:22 +0200

    Implementing nextGame function - still fetching all questions

--- src/pages/Games/Beginner/gameOne.jsx
+++ src/pages/Games/Beginner/gameOne.jsx
@@ -1,27 +1,31 @@
 import { Container, Button } from "react-bootstrap";
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { SET_CURRENT_GAME, setCurrentGame } from "../../../redux/actions/index.js";
+import { setCurrentGame } from "../../../redux/actions/index.js";
 
 const GameOne = () => {
-    let [game, setGame] = useState("")
+    let [currentGameIndex, setCurrentGameIndex] = useState(0); //stores the index of the current game beong displayed
+    let [currentGame, setGame] = useState(""); //stores the game object
 
     const games = useSelector((state) => {
         return state.currentGame;
     });
  
     const dispatch = useDispatch();
-    const navigate = useNavigate();
 
     useEffect(() => {
         dispatch(setCurrentGame());
+        setGame(games[currentGameIndex]) //setting the initial value of "game" to the first game in the list
     }, [])
 
     const nextGame = async () => {          
         try {
-            dispatch(setNextGame())
-            console.log("games ", games)
+            if (currentGameIndex < games.length - 1) { //checking if there are more games in the list and updating the state accordingly 
+                setCurrentGameIndex(currentGameIndex + 1);
+                setGame(games[currentGameIndex + 1]);
+            } else {
+                console.log("No more games");
+            }
         } catch (error) {
             console.log(error)  
         }
@@ -37,21 +41,21 @@ const GameOne = () => {
                     <div className="left-title"><h5>Questions</h5></div>
                     <div className="left-column">
                         <ul>
-                            {games.map(game => (
-                                <li key={game._id}>{game.question}</li>
-                            ))}
+                            {games.map(currentGame => (
+                            <li key={currentGame._id}>{currentGame.question}</li>
+                            ))}                            
                         </ul>
                     </div>
                     <div className="right-title"><h5>Answers</h5></div>
                     <div className="right-column">
                         <ul>
-                            {games.map((game) =>
-                            game.answers.map((answer) => 
-                            answer.answers.map((ans) => (
-                                <li key={ans}>{ans}</li>
+                            {games.map((currentGame) =>
+                                currentGame.answers.map((answer) =>
+                                    answer.answers.map((ans) => (
+                                        <li key={ans}>{ans}</li>
                             ))
-                            )    
-                        )}
+                            ))    
+                            }
                         </ul>
                     </div>
                 </div>
