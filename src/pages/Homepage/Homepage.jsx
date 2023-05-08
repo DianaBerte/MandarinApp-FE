d6a6ceca472af3b7f62a3542ac1cba17111aa1ab
DianaBerte <dianaberte.go@gmail.com>
2023-05-08 19:26:46 +0200

    Saves access token in local storage

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,15 +1,23 @@
 import { Button, Container } from "react-bootstrap"
-import { useNavigate } from "react-router-dom"
+import { useNavigate, useSearchParams } from "react-router-dom"
+import { useEffect } from "react"
 
 const Homepage = () => {
 
+    const [searchParams] = useSearchParams()
     const navigate = useNavigate()
+
+    useEffect(() => {
+        if (!localStorage.getItem("accessToken")) navigate("/login")
+        if (searchParams.get("accessToken")) {
+            localStorage.setItem("accessToken", searchParams.get("accessToken"))
+            navigate("/")
+        }
+    }, [navigate, searchParams])
 
     return(
         <>
         <Container>
-        <Button>Log in</Button>
-        <Button>Sign up</Button>
         {/* only after user's logged in or signed up appears Start Game btn */}
         <Button onClick={() => navigate("/ChooseYourLevel")}
         >Choose your level & Start!</Button>
