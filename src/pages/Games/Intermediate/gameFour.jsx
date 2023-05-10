4824ef91defcac4db5f3493e6b71a2228ddaac46
DianaBerte <dianaberte.go@gmail.com>
2023-05-10 10:43:24 +0200

    Status before implementing gameOne logic into gameFour

--- src/pages/Games/Intermediate/gameFour.jsx
+++ src/pages/Games/Intermediate/gameFour.jsx
@@ -4,7 +4,8 @@ const GameFour = () => {
         <>
         <h1>INTERMEDIATE / SECOND</h1>
         <div> <p>Audio: </p> <video src="https://res.cloudinary.com/degg5zebq/video/upload/v1683649820/Test_zgxftb.3gp" controls></video>
-        
+        {/* basically, the same things that happen in GameOne should happen here,
+        just that currentGame.question will be an audio instead of text  */}
         </div>
         </>
     )
