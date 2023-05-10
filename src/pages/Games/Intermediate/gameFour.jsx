65c633ea7f83384a2a582367e6a8ad2dd7374c7b
DianaBerte <dianaberte.go@gmail.com>
2023-05-10 10:52:08 +0200

    Audio from cloudinary rendered properly

--- src/pages/Games/Intermediate/gameFour.jsx
+++ src/pages/Games/Intermediate/gameFour.jsx
@@ -1,12 +1,95 @@
+import { Container, Button } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams, useNavigate } from "react-router-dom";
+import { fetchGames } from "../../../redux/actions/index.js";
+import RightAnswerModal from "../../../components/RightAnswerModal.jsx";
+import WrongAnswerModal from "../../../components/WrongAnswerModal.jsx";
 
 const GameFour = () => {
+
+    const {level} = useParams();
+    const {number} = useParams();
+    const navigate = useNavigate();
+
+    let [currentGameIndex, setCurrentGameIndex] = useState(0); //stores the index of the current game beong displayed
+    let [currentGame, setCurrentGame] = useState({}); //stores the game object
+    let [showRightAnsModal, setShowRightAnsModal] = useState(false);
+    let [showWrongAns, setShowWrongAns] = useState(false);
+    let [selectedAnswer, setSelectedAnswer] = useState(null)
+
+    const games = useSelector((state) => {
+        return state.currentGame;
+    });
+ 
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchGames(level, number));
+        setCurrentGame(games[currentGameIndex]) //setting the initial value of "game" to the first game in the list
+    }, [])
+
+    const nextExercise = async () => {          
+        try {
+            if (currentGameIndex < games.length - 1) { //checking if there are more games in the list and updating the state accordingly 
+                setCurrentGameIndex(currentGameIndex + 1);
+                setCurrentGame(games[currentGameIndex + 1]);
+            } else {
+                navigate(`/games/intermediate/third`)
+            }
+        } catch (error) {
+            console.log(error)  
+        }
+    };
+
+    useEffect(() => { //runs every time the "games" or "currentGameIndex" state changes, and updates the "currentGame" state to be the correct object based on the current index
+        setCurrentGame(games[currentGameIndex]);
+    }, [games, currentGameIndex]);  
+
+
+
+
     return(
         <>
-        <h1>INTERMEDIATE / SECOND</h1>
-        <div> <p>Audio: </p> <video src="https://res.cloudinary.com/degg5zebq/video/upload/v1683649820/Test_zgxftb.3gp" controls></video>
-        {/* basically, the same things that happen in GameOne should happen here,
-        just that currentGame.question will be an audio instead of text  */}
-        </div>
+        <Container>
+            <div className="container-div">
+                <div><h1>Match the pairs!</h1></div>
+                <div className="game-container">
+                    <div className="left-title"><h5></h5></div>
+                    <div className="left-column">
+                           <video src={currentGame.question} controls></video>
+                    </div>
+                    <div className="right-title"><h5></h5></div>
+                    <div className="right-column">
+                            {currentGame.answers && currentGame.answers.length > 0 //checking if the object "currentGame" has a property called "answers" and if it has elements in its "answers" array
+                                && currentGame.answers[0].answers.map((ans) => ( //If condition is met, it maps through the first element in the "answers" array and renders button with chars
+                                    <button className="chars-btn" onClick={() => {
+                                        setSelectedAnswer(ans);
+                                    }}key={ans}>{ans}</button>
+                                ))} {/* Else, it should render a button to the next page */}
+                    </div>
+                </div> 
+
+                <div className="btn-wrapper">                                
+                    <Button className="check-btn" onClick={() => {
+                        if(selectedAnswer === currentGame.answers[0].correctAnswer) {
+                             setShowRightAnsModal(true)
+                        } else {
+                             setShowWrongAns(true)
+                        }
+                        }}key={selectedAnswer}>Check
+                    </ Button>
+                </div>
+
+                <div className="btn-wrapper">
+                    <Button className="next-btn" onClick={nextExercise}> 
+                      Next
+                    </Button>
+                </div>
+            </div>
+        </Container>
+        <RightAnswerModal show={showRightAnsModal} onHide={() => setShowRightAnsModal(false)} />
+        <WrongAnswerModal show={showWrongAns} onHide={() => setShowWrongAns(false)}/>
         </>
     )
 }
