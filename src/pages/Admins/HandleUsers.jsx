bf39bd7f722311cf1c52a49e8f334581181734e4
DianaBerte <dianaberte.go@gmail.com>
2023-05-15 15:24:09 +0200

    handleUsers - about to implement action

--- src/pages/Admins/HandleUsers.jsx
+++ src/pages/Admins/HandleUsers.jsx
@@ -1,4 +1,4 @@
-import { Container, Col, Row, Form, Button, Modal, ListGroup } from "react-bootstrap"
+import { Container, Col, Row, Form, Button, Modal, ListGroup, ListGroupItem } from "react-bootstrap"
 import { getUsers } from "../../redux/actions/index.js"
 import { useEffect, useState } from "react"
 import { useDispatch } from "react-redux";
@@ -21,43 +21,25 @@ const HandleUsers = () => {
 
     return(
         <Container>
+            <div>
+            <h1>Handle Users</h1>
             {isLoading && <p>Loading...</p>}
+            <h2>List of users:</h2>
+            </div>
             {!isLoading && users.length > 0 ? (
                 users.map(user => (
-                <div key={user._id}>
-                    <h3>{user.firstName} {user.lastName} | {user.email} | Role: {user.role}</h3>
-                </div>
+                <Col key={user._id}>
+                    <ListGroup variant="flush">
+                    <Row>
+                    <ListGroup.Item className="listgroup-text">{user.firstName} {user.lastName}</ListGroup.Item>
+                    <ListGroup.Item className="listgroup-text">{user.email}</ListGroup.Item>
+                    <ListGroup.Item className="listgroup-text">Role: {user.role}</ListGroup.Item>
+                    <Button>Action</Button>
+                    </Row>
+                    </ListGroup>
+                </Col>
             ))) : null }
           </Container>
-        // {/* <Container>
-        //     <h1>Handle Users</h1>
-        //     {isLoading ? (
-        //         <h3>Loading...</h3>
-        //     ) : (
-        //         <>
-        //         <ListGroup variant="flush">
-        //             <ListGroup.Item className="listgroup-text">Name</ListGroup.Item>
-        //             <ListGroup.Item className="listgroup-text">Email Address</ListGroup.Item>
-        //             <ListGroup.Item className="listgroup-text">Action</ListGroup.Item>
-        //         </ListGroup>
-
-        //         <ListGroup>
-        //             {users.map(user => (
-        //                 <>
-        //                 <ListGroup.Item key={user.id} className="listgroup-text">{user.firstName}{user.lastName}</ListGroup.Item>
-        //                 <ListGroup.Item className="listgroup-text">{user.email}</ListGroup.Item>
-        //                 </>
-        //             ))}
-        //         </ListGroup>
-        //         </>
-        //     )}
-        //     <Col>
-        //     <Row>
-
-        //     </Row>
-        //     </Col>
-        // </Container> */}
-
     )
 }
 
