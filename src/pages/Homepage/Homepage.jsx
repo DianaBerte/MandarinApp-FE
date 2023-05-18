af32da97da6d5b80451391b99ed730e9cf7c16ec
DianaBerte <dianaberte.go@gmail.com>
2023-05-18 18:50:10 +0200

    Styles Choose your level page

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -20,21 +20,21 @@ const Homepage = () => {
     return(
         <>
         <NavbarComponent />
-        <Col col="4" md="3">
+        <Col col="4" md="3" sm="3" >
                     <div className="cloud-wrapper">
                         <img className="cloud" src="https://res.cloudinary.com/degg5zebq/image/upload/v1684424562/Untitled_design_2_tj6xty.png" alt="cloud" />
                     </div>
         </Col>
-        
+
         <Container className=" home-container">        
         <Row>
-            <Col col="10" md="6">
+            <Col col="10" md="6"  >
                     <Button className="choose-n-start-btn" onClick={() => navigate("/ChooseYourLevel")}
                     >Choose your level & Start!
                     </Button>
             </Col>
 
-                <Col col="4" md="6">
+                <Col col="4" md="6" xs="2">
                     <div className="riverli-wrapper">
                         <img className="riverLi" src="https://res.cloudinary.com/degg5zebq/image/upload/v1684416686/River_Li_p7a6zo.png" alt="River Li" />
                     </div>
