a2fe1617abaad06ba3e5f7fbad6dad43e2835937
DianaBerte <dianaberte.go@gmail.com>
2023-07-11 13:07:40 +0200

    Fixing homepage

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -20,53 +20,41 @@ const Homepage = () => {
     return(
         <>
         <NavbarComponent />
-
         <Container className="first-home-container">
-            <Row>
+            <Col>
             <div className="cloud-wrapper">
                 <img className="cloud" src="https://res.cloudinary.com/degg5zebq/image/upload/v1684424562/Untitled_design_2_tj6xty.png" alt="cloud" />
             </div>
-            </Row>
-        </Container>
 
-        <Container className="first-home-container">
             <Row>
-            <div className="temple-wrapper">
-                <img className="temple" src="https://res.cloudinary.com/degg5zebq/image/upload/v1684417100/Untitled_design_munqlw.png" />
-            </div>
-            </Row>
-        </Container>
-   
-        
 
-        <Container className="home-container">        
-        <Row>
-            <Col col="10" md="6"  >
-                    <Button className="choose-n-start-btn" onClick={() => navigate("/ChooseYourLevel")}
-                    >Choose your level & Start!
-                    </Button>
-            </Col>
+                <Col col="10" md="4">
+                    <div className="temple-wrapper">
+                        <img className="temple" src="https://res.cloudinary.com/degg5zebq/image/upload/v1684417100/Untitled_design_munqlw.png" />
+                    </div>
+                </Col>
 
-                {/* <Col col="4" md="6" xs="2">
+                <Col col="10" md="4"  >
+                        <Button className="choose-n-start-btn" onClick={() => navigate("/ChooseYourLevel")}
+                        >Choose your level & Start!
+                        </Button>
+                </Col>
+
+                <Col col="10" md="4">
+                    <div className="home-bamboo-wrapper">
+                        <img className="home-bamboo" src="https://res.cloudinary.com/degg5zebq/image/upload/v1684511851/Bamboo_1_s5isiv.png" />
+                    </div>
+                </Col>
+
+                <Col col="10" md="4">
                     <div className="riverli-wrapper">
                         <img className="riverLi" src="https://res.cloudinary.com/degg5zebq/image/upload/v1684416686/River_Li_p7a6zo.png" alt="River Li" />
                     </div>
-                </Col> */}
-        </Row>
+                </Col>
+
+            </Row>
+            </Col>
         </Container>
-
-        <Col >
-            <div className="home-bamboo-wrapper">
-                <img className="home-bamboo" src="https://res.cloudinary.com/degg5zebq/image/upload/v1684511851/Bamboo_1_s5isiv.png" />
-            </div>
-        </Col>
-
-        <Col >
-            <div className="riverli-wrapper">
-                <img className="riverLi" src="https://res.cloudinary.com/degg5zebq/image/upload/v1684416686/River_Li_p7a6zo.png" alt="River Li" />
-            </div>
-        </Col>
-
         </>
     )
 }
